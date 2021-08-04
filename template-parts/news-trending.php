f6a6e839dd6b49339eea6aa2f66a28ae6240c02a
Danial Bleile <danbleile@gmail.com>
2021-08-04 14:55:01 -0700

    Enhancement | Set trending post ids

--- template-parts/news-trending.php
+++ template-parts/news-trending.php
@@ -1,13 +1,20 @@
 <div class="wsu-callout wsu-news-callout wsu-news-callout--trending">
 <h2>Trending <span>News</span></h2>
 <?php
-$news_query = new \WP_Query( array( 'post_type' => 'news_article', 'posts_per_page' => 5 ) );
+$news_query = new \WP_Query(
+	array(
+		'post_type' => 'news_article',
+		'posts_per_page' => 5,
+		'post__in' => array(217848,217893,217764,217983,218072,218030),
+		'orderby' => 'post__in',
+		)
+	);
 
 while ( $news_query->have_posts() ) {
 
-    $news_query->the_post();
+	$news_query->the_post();
 
-    WSUWP\Theme\WDS\Template::render( 'template-parts/news-card-titles', get_post_type() );
+	WSUWP\Theme\WDS\Template::render( 'template-parts/news-card-titles', get_post_type() );
 
 };
 
