3c7498bacced93e3c53bacd1dd73e2939263b553
Danial Bleile <danbleile@gmail.com>
2021-12-06 16:07:36 -0800

    Enhancement | Add news article to category landing page

--- includes/press-release.php
+++ includes/press-release.php
@@ -48,6 +48,8 @@ class Press_Release {
 
 		add_action( 'init', array( __CLASS__, 'register_taxonomies' ), 12 );
 
+		add_action( 'pre_get_posts', array( __CLASS__, 'add_post_type_to_archive' ) );
+
 	}
 
 
@@ -101,6 +103,26 @@ class Press_Release {
 
 		return $url;
 
+	}
+
+
+	public static function add_post_type_to_archive( $query ) {
+
+		if ( $query->is_main_query() && ! is_admin() && ( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) ) {
+
+			$query_post_types = $query->get( 'post_type' );
+
+			if ( ! is_array( $query_post_types ) ) {
+
+				$query_post_types = array( $query_post_types );
+
+			}
+
+			$query_post_types[] = self::$slug;
+
+			$query->set( 'post_type', $query_post_types );
+
+		}
 	}
 
 
